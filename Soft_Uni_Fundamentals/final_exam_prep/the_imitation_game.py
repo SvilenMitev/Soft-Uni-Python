214c4810d185a92b549bd7adaecf7b20d8b5573c
svilen.mitev <smitev92@gmail.com>
2023-11-27 17:36:47 +0200

    exam prep

---
+++ Soft_Uni_Fundamentals/final_exam_prep/the_imitation_game.py
@@ -0,0 +1,31 @@
+
+
+
+initial_input = input()
+
+while True:
+    current_command = input()
+    if current_command == 'Decode':
+        break
+    splited_command = current_command.split('|')
+    command = splited_command[0] 
+    if command == 'Move':
+        numbers_to_move = int(splited_command[1])
+        initial_input   = initial_input[numbers_to_move:] + initial_input [:numbers_to_move]
+    elif command == 'Insert':
+        index = int(splited_command[1])
+        value = splited_command[2]
+        initial_input = initial_input[:index] + value + initial_input[index:]
+    elif command == 'ChangeAll':
+        substring = splited_command[1]
+        replacment = splited_command[2]
+        initial_input.replace(substring, replacment)
+        # result = ''
+        # for i in initial_input:
+        #     if i == substring:
+        #         result += replacment
+        #     else:
+        #         result += i
+        # initial_input = result
+
+print(initial_input)

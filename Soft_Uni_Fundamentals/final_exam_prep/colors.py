aa05ff7fd8428056cd2da2b1523ca9259543cafe
svilen.mitev <smitev92@gmail.com>
2023-12-04 17:33:49 +0200

    test

--- Soft_Uni_Fundamentals/final_exam_prep/colors.py
+++ Soft_Uni_Fundamentals/final_exam_prep/colors.py
@@ -2,9 +2,3 @@
 color_green = "\033[32m"
 color_white = "\033[0m"
 
-# test = {'test':["test"]}
-# test[test].append("second")
-# print(test)
-test = {'test': ["test2"]}
-test['test'].append("second")
-print(test)

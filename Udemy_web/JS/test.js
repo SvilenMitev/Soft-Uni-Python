4f00db9cf34117b4a9f79afd097486258788df22
Svilen Mitev <82656475+SvilenMitev@users.noreply.github.com>
2023-12-22 18:26:48 +0200

    js

---
+++ Udemy_web/JS/test.js
@@ -0,0 +1,9 @@
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+list: {
+  text += cars[0] + "<br>";
+  text += cars[1] + "<br>";
+  break list;
+  text += cars[2] + "<br>";
+  text += cars[3] + "<br>";
+}
+console.log(cars)
